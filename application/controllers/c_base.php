84eaae5dd92539bf5589e4eef37b7cbdfed9e6c9
sud0su <ahmat.raz@gmail.com>
2014-02-06 00:18:47 +0700

    tes

--- application/controllers/c_base.php
+++ application/controllers/c_base.php
@@ -1,22 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class c_base extends CI_Controller {
-<<<<<<< HEAD
-
 	public function index() 
 	{
 		$data['component'] = 'com_home/home';
 		$this->load->view('index',$data);
 	}
 }
-=======
-	
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
-}
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
->>>>>>> branch 'master' of https://github.com/sud0su/but-phon-that.git
