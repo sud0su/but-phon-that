ceeb6edad8e8a9e130aebe948f2f59fba8854511
sud0su <ahmat.raz@gmail.com>
2014-02-07 21:32:35 +0700

    form desain

--- application/views/com_instansi/view.php
+++ application/views/com_instansi/view.php
@@ -1,3 +1,14 @@
 <div class="data-content">
-	view instansi
+	<form id="v_instansi">
+		<label>	Nama Instansi</label>
+		<input type="text" id="instansi" /><br>
+		<label>	Nama Instansi</label>
+		<input type="text" id="instansi" /><br>
+		<label>	Nama Instansi</label>
+		<input type="text" id="instansi" /><br>
+		<label>	Nama Instansi</label>
+		<input type="text" id="instansi" /><br>
+		
+		<button id="simpan" type="button">Simpan</button>
+	</form>
 </div>
