3ac358de0b357f4052f6dbe69d4e300decce3abd
rzqbuchek <rzqbuchek@gmail.com>
2014-02-08 01:47:33 +0700

    Perubhaan com_kasus/data.php

--- application/views/com_kasus/data.php
+++ application/views/com_kasus/data.php
@@ -1,38 +1,38 @@
 <table id="TheTable" border="1" class="ExcelTable2007" width="100%">
 		<tr>
-			<th class="heading" width="20px"></th>
+			<th valign="bottom" width="20px"></th>
 			<th>No Kasus</th>
 			<th>Nama Kasus</th>
 			<th>action</th>
 		</tr>
 		<tr>
-			<td class="bottom">1</td>
-			<td class="heading">001</td>
-			<td valign="heading">Kasus Perkara Pemukulan</td>
-			<td valign="heading"></td>
+			<td class="heading">1</td>
+			<td valign="bottom">001</td>
+			<td valign="bottom">Kasus Perkara Pemukulan</td>
+			<td valign="bottom"></td>
 		</tr>
 		<tr>
-			<td class="bottom">1</td>
-			<td class="heading">002</td>
-			<td valign="heading">Kasus Perkara Pemukulan</td>
-			<td valign="heading"></td>
+			<td class="heading">1</td>
+			<td valign="bottom">002</td>
+			<td valign="bottom">Kasus Perkara Pemukulan</td>
+			<td valign="bottom"></td>
 		</tr>
 		<tr>
-			<td class="bottom">1</td>
-			<td class="heading">003</td>
-			<td valign="heading">Kasus Perkara Pemukulan</td>
-			<td valign="heading"></td>
+			<td class="heading">1</td>
+			<td valign="bottom">003</td>
+			<td valign="bottom">Kasus Perkara Pemukulan</td>
+			<td valign="bottom"></td>
 		</tr>
 		<tr>
-			<td class="bottom">1</td>
-			<td class="heading">004</td>
-			<td valign="heading">Kasus Perkara Pemukulan</td>
-			<td valign="heading"></td>
+			<td class="heading">1</td>
+			<td valign="bottom">004</td>
+			<td valign="bottom">Kasus Perkara Pemukulan</td>
+			<td valign="bottom"></td>
 		</tr>
 		<tr>
-			<td class="bottom">1</td>
-			<td class="heading">005</td>
-			<td valign="heading">Kasus Perkara Pemukulan</td>
-			<td valign="heading"></td>
+			<td class="heading">1</td>
+			<td valign="bottom">005</td>
+			<td valign="bottom">Kasus Perkara Pemukulan</td>
+			<td valign="bottom"></td>
 		</tr>
 	</table>
