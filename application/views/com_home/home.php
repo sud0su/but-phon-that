fb0db44cb75b4c4f9b6cacda3495c3293e5c89c9
sud0su <ahmat.raz@gmail.com>
2014-02-20 03:23:52 +0700

    baru
    desain baru

--- application/views/com_home/home.php
+++ application/views/com_home/home.php
@@ -1,82 +1,61 @@
-<div id="header" class="ui-widget-header ui-corner-tl ui-corner-tr" style="margin-bottom:2px;"> 
-	<a href="#" class="">
-		<img src="gudang/gambar/logo.png" width="50" height="50" class="ui-icon" alt="" /></a> 
-		<tr><span class="ui-accordion-header">e-Kekerasan</span></tr>
+<div class="header">
+    <span class="titleApp">
+     <img src="gudang/gambar/logo.png" />
+        e-Kekerasan
+    </span>
 </div>
-<div class="ui-front">
-	<div class="column-left">
-		<div class="content">
-			<div class="box-filter">
-				<!-- button menu -->
-				<div id="home"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Beranda</span>
-				</div>
-				
-				<div id="pro"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Propinsi</span>
-				</div>
-				
-				<div id="kab"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Kabupaten</span>
-				</div>
-				
-				<div id="ins"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Instansi</span>
-				</div>
-				
-				<div id="bid"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Bidang</span>
-				</div>
-				
-				<div id="kas"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Kasus</span>
-				</div>
-				
-				<div id="jns"
-					class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary menu"
-					role="button" aria-disabled="false">
-					<span class="ui-button-icon-primary ui-icon ui-icon-document"></span><span
-						class="ui-button-text">Jenis Data</span>
-				</div>
-				
-				<!-- end button menu -->
-			</div>
-		</div>
-	</div>
+<div id="column">
+    <div class="column-left">
+        <div class="content">
 
-	
-	
-	
-	<div class="column-right">
-		<div class="content">
-				<!-- load data -->
-				<div id="data">
-					<div id="loading"></div>
-				</div>
-				<!-- end load data -->
-		</div>
-	</div>
-<div id="footer" class="footer ui-widget-shadow ui-corner-br ui-corner-bl">
-	<a href='#'>Copy Right@eKekerasan</a>
-</div>
+            <div id="accordian">
+                <ul>
+                    <h3><a href="#" id="home">Beranda</a></h3>
+                    <li class="active">
+                        <h3><span class="icon-tasks"></span>Data Master</h3>
+                        <ul>
+                            <li><a href="#" id="pro">Provinsi</a></li>
+                            <li><a href="#" id="kab">Kabupaten</a></li>
+                            <li><a href="#" id="bid">Bidang</a></li>
+                            <li><a href="#" id="ins">Instansi</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><span class="icon-tasks"></span>Data Kasus</h3>
+                        <ul>
+                            <li><a href="#" id="kas">Kasus</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><span class="icon-tasks"></span>Data User</h3>
+                        <ul>
+                            <li><a href="#" id="user">User</a></li>
+                        </ul>
+                    </li>
+                    <h3><a href="#" id="logout">Log Out</a></h3>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="column-right">
+        <div class="content">
+            <!-- load data -->
+            <div id="data">
+                <div id="loading"></div>
+            </div>
+            <!-- end load data -->
+        </div>
+    </div>
 
 </div>
+
 <script src="gudang/app_js/data.js"></script>
+
+<div class="footer">
+    <span class="copy">
+        &copy; Dinas kekerasan perempuan aceh <?php echo date('Y')?>
+    </span>
+</div>
--- gudang/app_js/data.js
+++ gudang/app_js/data.js
@@ -1,27 +1,37 @@
 $(document).ready(function(){
-	tabs_data();
+    tabs_data();
 	
-	$('div#home').on('click',function(){
-		tabs_data();
-	});	
-	$('div#kab').on('click',function(){
-		kabupaten();
-	});	
-	$('div#pro').on('click',function(){
-		propinsi();
-	});
-	$('div#ins').on('click',function(){
-		instansi();
-	});
-	$('div#bid').on('click',function(){
-		bidang();
-	});
-	$('div#kas').on('click',function(){
-		kasus();
-	});
-	$('div#jns').on('click',function(){
-		jnsdata();
-	});
+    $('a#home').on('click',function(){
+        tabs_data();
+    });
+    $('a#kab').on('click',function(){
+        kabupaten();
+    });
+    $('a#pro').on('click',function(){
+        propinsi();
+    });
+    $('a#ins').on('click',function(){
+        instansi();
+    });
+    $('a#bid').on('click',function(){
+        bidang();
+    });
+    $('a#kas').on('click',function(){
+        kasus();
+    });
+    $('a#jns').on('click',function(){
+        jnsdata();
+    });
+
+    $("#accordian h3").click(function(){
+        //slide up all the link lists
+        $("#accordian ul ul").slideUp();
+        //slide down the link list below the h3 clicked - only if its closed
+        if(!$(this).next().is(":visible"))
+        {
+            $(this).next().slideDown();
+        }
+    })
 
 });
 
@@ -30,78 +40,78 @@ $(window).bind("load", function() {
 });
 
 function tabs_data(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_tab",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 }
 
 function kabupaten(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_kab",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
 
 function propinsi(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_pro",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
 
 function instansi(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_ins",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
 
 function bidang(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_bid",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
 
 function kasus(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_kasus",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
 
 function jnsdata(){
-	$.ajax({
+    $.ajax({
         type: "POST",
         url : BASE_URL+"c_base/view_jnsdata",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(data){
-        	 	$('#data').html(data);
+            $('#data').html(data);
         }
-	});
+    });
 };
